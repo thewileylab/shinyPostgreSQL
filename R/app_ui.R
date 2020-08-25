3b6a4b72732c522fda6a26dced9b9fe220e791aa
David Mayer <david.mayer@cuanschutz.edu>
2020-08-25 17:10:32 -0600

    add deps

--- R/app_ui.R
+++ R/app_ui.R
@@ -34,9 +34,11 @@ golem_add_external_resources <- function(){
     bundle_resources(
       path = app_sys('app/www'),
       app_title = 'shinyPostgreSQL'
-    )
+    ),
     # Add here other external resources
-    # for example, you can add shinyalert::useShinyalert() 
+    # for example, you can add shinyalert::useShinyalert()
+    shinyjs::useShinyjs(),
+    shinyWidgets::useShinydashboard(),
   )
 }
 
