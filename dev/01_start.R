41d5d46c7abfb1b01657919e2f39084a36c5443e
David Mayer <david.mayer@cuanschutz.edu>
2020-08-25 16:58:22 -0600

    Initial commit

---
+++ dev/01_start.R
@@ -0,0 +1,60 @@
+# Building a Prod-Ready, Robust Shiny Application.
+# 
+# README: each step of the dev files is optional, and you don't have to 
+# fill every dev scripts before getting started. 
+# 01_start.R should be filled at start. 
+# 02_dev.R should be used to keep track of your development during the project.
+# 03_deploy.R should be used once you need to deploy your app.
+# 
+# 
+########################################
+#### CURRENT FILE: ON START SCRIPT #####
+########################################
+
+## Fill the DESCRIPTION ----
+## Add meta data about your application
+golem::fill_desc(
+  pkg_name = "shinyPostgreSQL", # The Name of the package containing the App 
+  pkg_title = "shinyPostgreSQL: Access a PostgreSQL database through R Shiny", # The Title of the package containing the App 
+  pkg_description = "A shiny module to authenticate your R Shiny Application with a PostgreSQL Database.", # The Description of the package containing the App 
+  author_first_name = "David", # Your First Name
+  author_last_name = "Mayer", # Your Last Name
+  author_email = "david.mayer@cuanschutz.edu", # Your Email
+  repo_url = NULL # The URL of the GitHub Repo (optional) 
+)     
+
+## Set {golem} options ----
+golem::set_golem_options()
+
+## Create Common Files ----
+## See ?usethis for more information
+usethis::use_mit_license( name = "Golem User" )  # You can set another license here
+usethis::use_readme_rmd( open = FALSE )
+usethis::use_code_of_conduct()
+usethis::use_lifecycle_badge( "Experimental" )
+usethis::use_news_md( open = FALSE )
+
+## Use git ----
+usethis::use_git()
+
+## Init Testing Infrastructure ----
+## Create a template for tests
+golem::use_recommended_tests()
+
+## Use Recommended Packages ----
+golem::use_recommended_deps()
+
+## Favicon ----
+# If you want to change the favicon (default is golem's one)
+golem::use_favicon() # path = "path/to/ico". Can be an online file. 
+golem::remove_favicon()
+
+## Add helper functions ----
+golem::use_utils_ui()
+golem::use_utils_server()
+
+# You're now set! ----
+
+# go to dev/02_dev.R
+rstudioapi::navigateToFile( "dev/02_dev.R" )
+
