3b8adfa67012d4bf1480818d1e6d0d84d3031b07
David Mayer <david.mayer@cuanschutz.edu>
2020-08-26 09:24:18 -0600

    add basic connection error handling - when connection is valid, store user inputs

--- R/mod_PostgreSQL_setup.R
+++ R/mod_PostgreSQL_setup.R
@@ -58,17 +58,45 @@ postgresql_setup_server <- function(id) {
         ui = shinyPostgreSQL::postgresql_setup_ui(id),
         ### Connection Variables
         is_connected = 'no',
-        db_con = NULL
+        db_con = NULL,
+        dbname = NULL,
+        host = NULL,
+        port = NULL,
+        user = NULL,
+        password = NULL
         )
+      
+      ## Observe Connect Button ----
       observeEvent(input$connect,{
-        postgresql_setup$db_con <- DBI::dbConnect(RPostgres::Postgres(),
-                                                  dbname = input$dbname, 
-                                                  host = input$host, # i.e. 'ec2-54-83-201-96.compute-1.amazonaws.com'
-                                                  port = input$port, # or any other port specified by your DBA
-                                                  user = input$user,
-                                                  password = input$password
-                                                  )
-          })
+        postgresql_setup$db_con <- tryCatch({
+          DBI::dbConnect(RPostgres::Postgres(),
+                         dbname = input$dbname, 
+                         host = input$host, # i.e. 'ec2-54-83-201-96.compute-1.amazonaws.com'
+                         port = input$port, # or any other port specified by your DBA
+                         user = input$user,
+                         password = input$password
+                         )
+          }, warning = function(w) {
+            message(glue::glue('{w}'))
+            return('connection_warning')
+          }, error = function(e) {
+            message(glue::glue('{e}'))
+            return('connection_error')
+            }
+          )
+        })
+      
+      ## Monitor DBI Connection Object  ----
+      ### Check for valid connection information
+      observeEvent(postgresql_setup$db_con, {
+        if(postgresql_setup$db_con %>% class() == 'PostgreSQLConnection') {
+          postgresql_setup$is_connected <- 'yes'
+          postgresql_setup$dbname <- input$dbname
+          postgresql_setup$host <- input$host
+          postgresql_setup$port <- input$port
+          postgresql_setup$user <- input$user
+        }
+      })
       return(postgresql_setup)
       }
     )
